5e27f2a2df927bb0471009d526e15a4e8f4f0cb4
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2023-12-20 13:11:56 +0600

    react hook form initiated

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
 import signUpAnimation from "../../assets/signup.json"
 import Lottie from "lottie-react";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {register, handleSubmit, formState:{errors}}= useForm()
+  const handleSignUp = (data) =>{
+    console.log(data)
+  }
     return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,31 +16,69 @@ const SignUp = () => {
       <Lottie animationData={signUpAnimation}></Lottie>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
+      <form onSubmit={handleSubmit(handleSignUp)} className="card-body">
       <h1 className="text-5xl font-bold pl-4">Sign Up!</h1>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="name" placeholder="name" className="input input-bordered" {...register("name",{
+            required:{
+              value:true,
+              message: "You should provide your name"
+            }
+          })} />
+          {
+            errors.name && (
+              <p className="text-error mt-2 text-start"> {errors?.name.message}</p>
+            )
+          }
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" placeholder="email" className="input input-bordered" {...register("email",{
+            required:{
+              value: true,
+              message: "You must provide email address"
+            }
+          })} />
+          {
+            errors.email && (
+              <p className="text-error mt-2 text-start"> {errors?.email.message}</p>
+            )
+          }
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" placeholder="password" className="input input-bordered" {...register("password",{
+            required:{
+              value: true,
+              message: "You must provide password"
+            },
+            minLength:{
+              value: 8,
+              message: "Password must be 8 character long"
+            },
+            pattern:{
+              value: /(?=[A-Z])/,
+              message: "Password must have uppercase character"
+            }
+          })} />
+          {
+            errors.password && (
+              <p className="text-error mt-2 text-start"> {errors?.password.message}</p>
+            )
+          }
           <label className="label">
             <Link to="/signin" className="label-text-alt link link-hover">Already have an account? Please Login</Link>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Sign up</button>
+          <input type="submit" className="btn btn-primary"  value={"sign up"} />
         </div>
         <div className="divider">OR</div>
       <button className="btn btn-outline">Sign in with google</button>
